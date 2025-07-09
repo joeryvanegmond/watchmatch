401d99694f3f0fccb9e10dfa9cce83f7fbff1f6c
Joery van Egmond <joeryvanegmond@hotmail.com>
2025-07-10 00:17:45 +0200

    nog kleine optimalizatie images (zoekterm)

--- app/Services/WatchImageService.php
+++ app/Services/WatchImageService.php
@@ -25,9 +25,9 @@ class WatchImageService
         }
         
         try {
-            $variant = $watch->variant ?? '';
+            $variant = $watch->variant;
             // $image = $this->googleSearch->searchImage("{$watch->brand} {$watch->model} {$variant}");
-            $result = $this->quack->getFirstDuckDuckGoImage("{$watch->brand} {$watch->model} {$variant}");
+            $result = $this->quack->getFirstDuckDuckGoImage("{$watch->brand} {$watch->model} {$variant} watch");
             if ($result['image'] != null) {
                 $watch->image_url = $result['image'];
             }
