96eb1a22d6da2d36e5a8c499e6df833f1873609c
Joery van Egmond <joeryvanegmond@hotmail.com>
2025-11-02 20:40:09 +0100

    zo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <div class="row text-white">
     <div class="mb-4 d-flex flex-row justify-content-around">
-        <span class="text-center small" style="font-size: 0.75rem;">{{number_format($brandCount)}} brands</span>
+        {{-- <span class="text-center small" style="font-size: 0.75rem;">{{number_format($brandCount)}} brands</span> --}}
         <span class="text-center small" style="font-size: 0.75rem;">{{number_format($watchCount)}} watches</span>
         <span class="text-center small" style="font-size: 0.75rem;">{{number_format($imageCount)}} images</span>
         <span class="text-center small" style="font-size: 0.75rem;">{{number_format($similarityCount)}} similarities</span>
